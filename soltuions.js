bc478534f1bbe8a2c5039e948f6db0077965e6b9
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-06-23 23:48:40 +0530

    fccjs

---
+++ soltuions.js
@@ -0,0 +1,25 @@
+//addition
+const sum = 10 + 10;
+sum = 20;
+
+//substraction
+const difference = 45 - 33;
+difference = 12;
+
+//multiplication
+const product = 8 * 10;
+product = 80;
+
+// divide
+
+const quotient = 66 / 33;
+quotient = 2;
+
+// increment
+
+/*Change the code to use the ++ operator on myVar.*/
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+88;
