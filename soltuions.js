c7b260672b5957cf3e8e2114cff9c6a4e32ef824
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-06-26 13:24:53 +0530

    strings

--- soltuions.js
+++ soltuions.js
@@ -23,3 +23,28 @@ let myVar = 87;
 // Only change code below this line
 myVar++;
 88;
+
+// escaping quotes
+var myStr = 'I am a "double quoted" string inside "double quotes".';
+
+//Quoting Strings with Single Quotes
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Escape Sequences in Strings
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+//Concatenating Strings with Plus Operator
+
+const myStr = "This is the start." + " This is the end.";
+
+//Concatenating Strings with the Plus Equals Operator
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//Constructing Strings with Variables
+
+const myName = "Archit Dadwal";
+const myStr = "My name is " + myName + " and I am well";
