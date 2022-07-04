c7a7a51aa055a89b6d0b0e7579324294406206bf
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-04 18:19:19 +0530

    66

--- soltuions.js
+++ soltuions.js
@@ -576,3 +576,66 @@ function orderMyLogic(val) {
 }
 
 orderMyLogic(7);
+
+/*Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge
+*/
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else num >= 20;
+  {
+    return "Huge";
+  }
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+
+//Golf Code
+
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes === par - 1) {
+    return names[2];
+  } else if (strokes === par) {
+    return names[3];
+  } else if (strokes === par + 1) {
+    return names[4];
+  } else if (strokes === par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+  // Only change code above this line
+}
+
+golfScore(5, 4);
