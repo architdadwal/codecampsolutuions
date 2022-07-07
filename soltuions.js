a6f97b1063d2305fbe13791d28055efe2d39b513
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-07 13:09:31 +0530

    added new solutions

--- soltuions.js
+++ soltuions.js
@@ -780,3 +780,13 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+//Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
