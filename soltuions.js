a4b9cffba0fc5b0d36bb6caf569d1477afd58596
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-12 23:40:19 +0530

    nextsol

--- soltuions.js
+++ soltuions.js
@@ -992,3 +992,24 @@ function checkObj(obj, checkProp) {
   }
 }
 // Only change code above this line
+
+//Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+
+    gold: true,
+  },
+  {
+    artist: "BABU RAI",
+    title: "BOOM",
+    release_year: 1998,
+    formats: ["CD", "8T", "LP"],
+
+    gold: true,
+  },
+];
