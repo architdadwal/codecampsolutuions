7b8bf2a0fe2b67d3ac202b8b254630fc63fbf931
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-20 23:53:54 +0530

    added more solutions

--- soltuions.js
+++ soltuions.js
@@ -1029,3 +1029,17 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];
+//Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
