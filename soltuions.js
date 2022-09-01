90195e984c99a9ddccadcf62c3ba4eaea119e7d3
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-09-01 11:37:19 +0530

    adding more solutions

--- soltuions.js
+++ soltuions.js
@@ -1058,3 +1058,18 @@ const myPlants = [
 ];
 
 const secondTree = myPlants[1].list[1];
+
+//Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
